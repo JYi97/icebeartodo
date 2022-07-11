a358fdf5ad388a6b50f76094187ec153a7a1e2ca
justin <jyi@ucsb.edu>
2022-07-11 14:59:29 -0700

    Click edit button to show edit form

--- react-app/src/components/ActivityDetailsPage/index.js
+++ react-app/src/components/ActivityDetailsPage/index.js
@@ -29,7 +29,7 @@ const ActivityDetailsPage = () => {
 
     console.log("THIS IS THE SPECIFIC ACTIVITY", activity?.folderId)
 
-    const folderId = activity?.folderId
+    
 
     useEffect(() => {
 
