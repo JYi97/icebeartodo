3304d6a8cb945e1f4ae1b7ed4facbf87d9b40c87
justin <jyi@ucsb.edu>
2022-07-12 23:51:38 -0700

    Splash page will only show to non-logged in users

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,12 +1,16 @@
 import './splashpage.css'
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 const SplashPage = () => {
+    const currentUser = useSelector(state => state.session.user)
+    const history = useHistory()
     return (
         <>
-            <h3>
+        {currentUser ? history.push('/home') : <h3>
                 Organize your chores and schedule so you can not only
                  get your work done but have time to relax like Ice Bear.
-            </h3>
+            </h3> }
         </>
     )
 }
